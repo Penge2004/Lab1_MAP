369059789a15152b88ab084519f521a72743be81
NagyBalazsMark <nagybalazsmark@gmail.com>
2024-10-08 08:55:24 +0300

    The first exercise is done with the grades

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
         UniversityGrading universityGrading = new UniversityGrading();
         int[] grades = new int[]{29,37,38,41,84,67};
-        System.out.println(universityGrading.failedNotes(grades));
+        System.out.println(Arrays.toString(universityGrading.failedNotes(grades)));
+        System.out.println(universityGrading.average(grades));
+        System.out.println(Arrays.toString(universityGrading.round(grades)));
+        System.out.println(universityGrading.maxGradeAfterRound(grades));
 
     }
 }
--- src/UniversityGrading.java
+++ src/UniversityGrading.java
@@ -1,13 +1,64 @@
+import java.text.DecimalFormat;
+
 public class UniversityGrading {
     public int[] failedNotes(int[] grades){
-        int[] failed = new int[1];
-        int j = 0;
+        int[] failed = new int[0];
         for(int i = 0; i < grades.length; i++){
             if(grades[i] <= 40){
-                failed[j] = grades[i];
-                j++;
+               failed = append(grades[i], failed);
             }
         }
         return failed;
+    }
+
+    private int[] append(int newElem, int[] oldArray){
+        int[] newArray = new int[oldArray.length + 1];
+        for(int i = 0; i < oldArray.length; i++){
+            newArray[i] = oldArray[i];
+        }
+        newArray[oldArray.length] = newElem;
+        return newArray;
+    }
+
+    public double average(int[] grades){
+        int sum = 0;
+        for(int i = 0; i < grades.length; i++){
+            sum += grades[i];
+        }
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        double average = (double)sum / grades.length;
+
+        return Double.parseDouble(df.format(average));
+    }
+
+    public int[] round(int[] grades){
+        int[] newArray = new int[grades.length];
+        for(int i = 0; i < grades.length; i++){
+            if (grades[i] < 38)
+                newArray[i] = grades[i];
+            else if(grades[i] % 10 == 8 || grades[i] % 10 == 9 ||
+                    grades[i] % 10 == 3 || grades[i] % 10 == 4)
+                newArray[i] = roundToFive(grades[i]);
+            else newArray[i] = grades[i];
+        }
+        return newArray;
+    }
+
+    private int roundToFive(int number){
+        int reminder = number % 5;
+        if (reminder == 0)
+            return number;
+        else return number + 5 - reminder;
+    }
+
+    public int maxGradeAfterRound(int[] grades){
+        int max = Integer.MIN_VALUE;
+        grades = round(grades);
+        for(int i = 0; i < grades.length; i++){
+            if(grades[i] > max)
+                max = grades[i];
+        }
+        return max;
     }
 }
